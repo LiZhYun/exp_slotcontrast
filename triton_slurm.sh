d389e79c488be67de5829abd51ddbe4317015d6e
LiZhYun <18583960999@163.com>
2025-11-10 10:41:40 +0200

    test ideas on slotcontrast

---
+++ triton_slurm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=slotcontrast_train
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --time=0-60:00:00
+#SBATCH --gres=gpu:a100:1
+#SBATCH --output=logs/train_%j.out
+#SBATCH --error=logs/train_%j.err
+
+module load mamba
+module load triton-dev/2025.1-gcc
+module load gcc/13.3.0
+module load cuda/12.6.2
+export HF_HOME=/$WRKDIR/.huggingface_cache
+export TORCH_DISTRIBUTED_DEBUG=DETAIL
+export TORCH_EXTENSIONS_DIR=$WRKDIR/torch_extensions
+
+source activate slotcontrast
+
+DATA_DIR="/scratch/work/liz23/POST3R/post3r/data"
+OUTPUT_DIR="/scratch/work/liz23/slotcontrast/logs"
+
+srun python slotcontrast/train.py "configs/slotcontrast/ytvis2021.yaml" \
+        --data-dir ${DATA_DIR} \
+        --log-dir ${OUTPUT_DIR} \
