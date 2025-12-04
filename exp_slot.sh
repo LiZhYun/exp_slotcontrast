cedd365def03aa752f6bb915ff5abfff7207bfaa
LiZhYun <zhiyuan.li@aalto.fi>
2025-12-04 20:29:13 +0200

    add cycle loss

--- exp_slot.sh
+++ exp_slot.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
 
 # Define experiment configurations
-# Format: "experiment_name use_ttt3r use_gated use_gated_predictor use_ttt use_gru loss_ss"
+# Format: "experiment_name use_ttt3r use_gated use_gated_predictor use_ttt use_gru loss_ss loss_cycle"
 CONFIGS=(
-    # "baseline_w_contrast false false false false true 0.5"
-    # "baseline_wo_contrast false false false false true 0.0"
+    # "baseline_w_contrast false false false false true 0.5 0.0"
+    # "baseline_wo_contrast false false false false true 0.0 0.0"
 
-    # "gated_attention_grouper_w_contrast false true false false false 0.5" # may work
-    # "gated_attention_grouper_wo_contrast false true false false false 0.0"
+    # "gated_attention_grouper_w_contrast false true false false false 0.5 0.0" # may work
+    # "gated_attention_grouper_wo_contrast false true false false false 0.0 0.0"
 
-    "gated_attention_grouper_gru_w_contrast false true false false true 0.5"
+#     "gated_attention_grouper_gru_w_contrast false true false false true 0.5 0.0"
 
-    # "gated_attention_predictor_w_contrast false false true false true 0.5"
-    # "gated_attention_predictor_wo_contrast false false true false true 0.0" 
+    # "gated_attention_predictor_w_contrast false false true false true 0.5 0.0"
+    # "gated_attention_predictor_wo_contrast false false true false true 0.0 0.0" 
 
-    # "gated_attention_grouper_predictor_w_contrast false true true false false 0.5" # may work
-    # "gated_attention_grouper_predictor_wo_contrast false true true false false 0.0"
+    # "gated_attention_grouper_predictor_w_contrast false true true false false 0.5 0.0" # may work
+    # "gated_attention_grouper_predictor_wo_contrast false true true false false 0.0 0.0"
 
-    "gated_attention_grouper_predictor_gru_w_contrast false true true false true 0.5"
+#     "gated_attention_grouper_predictor_gru_w_contrast false true true false true 0.5 0.0"
 
-    # "ttt3r_grouper_w_contrast false false false true false 0.5"
-    # "ttt3r_grouper_wo_contrast false false false true false 0.0"
+    # "ttt3r_grouper_w_contrast false false false true false 0.5 0.0"
+    # "ttt3r_grouper_wo_contrast false false false true false 0.0 0.0"
 
-    # "ttt3r_predictor_w_contrast true false false false true 0.5"
-    # "ttt3r_predictor_wo_contrast true false false false true 0.0"
+    # "ttt3r_predictor_w_contrast true false false false true 0.5 0.0"
+    # "ttt3r_predictor_wo_contrast true false false false true 0.0 0.0"
 
-    # "ttt3r_grouper_predictor_w_contrast true false false true false 0.5" # may work
-    # "ttt3r_grouper_predictor_wo_contrast true false false true false 0.0"
+    # "ttt3r_grouper_predictor_w_contrast true false false true false 0.5 0.0" # may work
+    # "ttt3r_grouper_predictor_wo_contrast true false false true false 0.0 0.0"
 
-    "ttt3r_grouper_predictor_gru_w_contrast true false false true true 0.5"
+#     "ttt3r_grouper_predictor_gru_w_contrast true false false true true 0.5 0.0"
+
+    "loss_cycle_w_contrast false false false false true 0.5 0.5"
+    "loss_cycle_wo_contrast false false false false true 0.0 0.5"
 )
 
 for config in "${CONFIGS[@]}"; do
-    read -r exp_name use_ttt3r use_gated use_gated_predictor use_ttt use_gru loss_ss <<< "$config"
-    
+    read -r exp_name use_ttt3r use_gated use_gated_predictor use_ttt use_gru loss_ss loss_cycle <<< "$config"
+
     echo "Submitting: $exp_name"
     
     sbatch --job-name="sc_${exp_name}" triton_slurm.sh \
@@ -43,7 +46,8 @@ for config in "${CONFIGS[@]}"; do
         "model.predictor.use_gated=${use_gated_predictor}" \
         "model.grouper.use_ttt=${use_ttt}" \
         "model.grouper.use_gru=${use_gru}" \
-        "model.loss_weights.loss_ss=${loss_ss}"
+        "model.loss_weights.loss_ss=${loss_ss}" \
+        "model.loss_weights.loss_cycle=${loss_cycle}"
 done
 
 echo "All jobs submitted!"
