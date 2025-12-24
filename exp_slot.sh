aa4dd1a5d7656231ec1957a4b28fa4b0a9bec2b0
LiZhYun <18583960999@163.com>
2025-12-24 11:17:46 +0200

    test hungarian

--- exp_slot.sh
+++ exp_slot.sh
@@ -8,27 +8,63 @@ CONFIG_FILE="configs/slotcontrast/movi_e.yaml"
 # Format: "experiment_name use_ttt3r use_gated use_gated_predictor use_ttt use_gru loss_ss loss_cycle window_size"
 CONFIGS=(
 
-    "baseline_w_contrast 0.5 FixedLearnedInit first_frame networks.TransformerEncoder false false"
-    "baseline_wo_contrast 0.0 FixedLearnedInit first_frame networks.TransformerEncoder false false"
+    # "baseline_w_contrast 0.5 FixedLearnedInit first_frame networks.TransformerEncoder false false 2 3"
+    # "baseline_wo_contrast 0.0 FixedLearnedInit first_frame networks.TransformerEncoder false false 2 3"
 
-    "no_predictor_w_contrast 0.5 FixedLearnedInit first_frame networks.TransformerEncoder false true"
-    "no_predictor_wo_contrast 0.0 FixedLearnedInit first_frame networks.TransformerEncoder false true"
+    # "no_predictor_w_contrast 0.5 FixedLearnedInit first_frame networks.TransformerEncoder false true 2 3"
+    # "no_predictor_wo_contrast 0.0 FixedLearnedInit first_frame networks.TransformerEncoder false true 2 3"
 
-    "per_frame_no_predictor_w_contrast 0.5 FixedLearnedInit per_frame networks.TransformerEncoder false true"
-    "per_frame_no_predictor_wo_contrast 0.0 FixedLearnedInit per_frame networks.TransformerEncoder false true"
+    # "no_predictor_w_contrast 0.5 FixedLearnedInit first_frame networks.TransformerEncoder false true 1 1"
+    # "no_predictor_wo_contrast 0.0 FixedLearnedInit first_frame networks.TransformerEncoder false true 1 1"
 
-    "greedy_no_predictor_w_contrast 0.5 GreedyFeatureInit per_frame networks.TransformerEncoder false true"
-    "greedy_no_predictor_wo_contrast 0.0 GreedyFeatureInit per_frame networks.TransformerEncoder false true"
+    # "per_frame_no_predictor_w_contrast 0.5 FixedLearnedInit per_frame networks.TransformerEncoder false true 2 3"
+    # "per_frame_no_predictor_wo_contrast 0.0 FixedLearnedInit per_frame networks.TransformerEncoder false true 2 3"
 
-    "greedy_hungarian_w_contrast 0.5 GreedyFeatureInit per_frame networks.HungarianPredictor false false"
-    "greedy_hungarian_wo_contrast 0.0 GreedyFeatureInit per_frame networks.HungarianPredictor false false"
+    # "greedy_no_predictor_w_contrast 0.5 GreedyFeatureInit per_frame networks.TransformerEncoder false true 2 3"
+    # "greedy_no_predictor_wo_contrast 0.0 GreedyFeatureInit per_frame networks.TransformerEncoder false true 2 3"
+
+    # "greedy_hungarian_w_contrast 0.5 GreedyFeatureInit per_frame networks.HungarianPredictor false false 2 3"
+    # "greedy_hungarian_wo_contrast 0.0 GreedyFeatureInit per_frame networks.HungarianPredictor false false 2 3"
+
+    # "greedy_no_predictor_w_contrast 0.5 GreedyFeatureInit per_frame networks.TransformerEncoder false true 1 1"
+    # "greedy_no_predictor_wo_contrast 0.0 GreedyFeatureInit per_frame networks.TransformerEncoder false true 1 1"
+
+    # "per_frame_no_predictor_w_contrast 0.5 FixedLearnedInit per_frame networks.TransformerEncoder false true 1 1"
+    # "per_frame_no_predictor_wo_contrast 0.0 FixedLearnedInit per_frame networks.TransformerEncoder false true 1 1"
+
+    # "baseline_w_contrast 0.5 FixedLearnedInit first_frame networks.TransformerEncoder false false 1 1"
+    # "baseline_wo_contrast 0.0 FixedLearnedInit first_frame networks.TransformerEncoder false false 1 1"
+
+    # "first_frame_greedy_w_contrast 0.5 GreedyFeatureInit first_frame networks.TransformerEncoder false false 2 3"
+    # "first_frame_greedy_wo_contrast 0.0 GreedyFeatureInit first_frame networks.TransformerEncoder false false 2 3"
+
+    # "first_frame_greedy_w_contrast 0.5 GreedyFeatureInit first_frame networks.TransformerEncoder false false 1 1"
+    # "first_frame_greedy_wo_contrast 0.0 GreedyFeatureInit first_frame networks.TransformerEncoder false false 1 1"
+
+    # "first_frame_greedy_no_predictor_w_contrast 0.5 GreedyFeatureInit first_frame networks.TransformerEncoder false true 2 3"
+    # "first_frame_greedy_no_predictor_wo_contrast 0.0 GreedyFeatureInit first_frame networks.TransformerEncoder false true 2 3"
+
+    # "first_frame_greedy_no_predictor_w_contrast 0.5 GreedyFeatureInit first_frame networks.TransformerEncoder false true 1 1"
+    # "first_frame_greedy_no_predictor_wo_contrast 0.0 GreedyFeatureInit first_frame networks.TransformerEncoder false true 1 1"
+
+    # "first_frame_greedy_hungarian_w_contrast 0.5 GreedyFeatureInit first_frame networks.HungarianPredictor false false 2 3"
+    # "first_frame_greedy_hungarian_wo_contrast 0.0 GreedyFeatureInit first_frame networks.HungarianPredictor false false 2 3"
+
+    # "first_frame_greedy_hungarian_w_contrast 0.5 GreedyFeatureInit first_frame networks.HungarianPredictor false false 1 1"
+    # "first_frame_greedy_hungarian_wo_contrast 0.0 GreedyFeatureInit first_frame networks.HungarianPredictor false false 1 1"
+
+    # "first_frame_greedy_w_contrast 0.5 GreedyFeatureInit first_frame networks.TransformerEncoder false false 2 3 true vit_block11"
+    # "first_frame_greedy_wo_contrast 0.0 GreedyFeatureInit first_frame networks.TransformerEncoder false false 2 3 true vit_block11"
+
+    "per_frame_greedy_hungarian_w_contrast 0.5 GreedyFeatureInit per_frame networks.HungarianPredictor false false 2 3 false vit_block12"
+    "per_frame_greedy_hungarian_wo_contrast 0.0 GreedyFeatureInit per_frame networks.HungarianPredictor false false 2 3 false vit_block12"
 
     )
 
 SEED=42
     
 for config in "${CONFIGS[@]}"; do
-    read -r exp_name loss_ss init_name init_mode predictor skip_corrector skip_predictor <<< "$config"
+    read -r exp_name loss_ss init_name init_mode predictor skip_corrector skip_predictor n_iters f_n_iters use_gated features <<< "$config"
 
     echo "Submitting: $exp_name with config: $CONFIG_FILE"
     
@@ -40,6 +76,10 @@ for config in "${CONFIGS[@]}"; do
         "model.predictor.name=${predictor}" \
         "model.latent_processor.skip_corrector=${skip_corrector}" \
         "model.latent_processor.skip_predictor=${skip_predictor}" \
+        "model.grouper.n_iters=${n_iters}" \
+        "model.latent_processor.first_step_corrector_args.n_iters=${f_n_iters}" \
+        "model.grouper.use_gated=${use_gated}" \
+        "model.encoder.backbone.features=${features}" \
         "seed=${SEED}"
 done
 
